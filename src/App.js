a6ddd5c8b7b67dbcf1d92b258c2c5afe7b77e489
GyeDan <mlldaniel.hwang@gmail.com>
2021-05-03 21:47:18 +0900

    2nd commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With Me
+          Learn React With Daniel
         </a>
       </header>
     </div>
